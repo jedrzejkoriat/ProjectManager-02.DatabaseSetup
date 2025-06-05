e21b98ebed482e081ad63f872d901357cba13a08
jedrzejkoriat <jedrzej.koriat@gmail.com>
2025-06-06 01:10:40 +0200

    feat: modify permissions

--- DatabaseScripts/004_InsertInitialData.sql
+++ DatabaseScripts/004_InsertInitialData.sql
@@ -1,19 +1,19 @@
 USE ProjectManagerDB;
 
 INSERT INTO Permissions (Id, Name) VALUES
-('d2f6a7ce-15e3-45df-9a2a-7b1eec0042a3', 'Comment'),
-('e5cc3f04-a0cf-4d95-ae7c-9d3b76247be6', 'AddProjectMember'),
-('f4a25e30-6d9e-4e3a-8a4c-0b1de8dfad5a', 'EditProjectMember'),
-('a8b3e297-bfb2-464f-8fa6-ffba0bcaa2b4', 'RemoveProjectMember'),
-('39f59bc3-2be4-4e45-bfb0-c80f4cb1bfe3', 'AddTag'),
-('1d1b1a99-f875-47fa-a6dc-786db14ed4da', 'EditTag'),
-('a9d231b0-6f35-46c3-b8d5-15a576a31371', 'RemoveTag'),
-('cc50c1e2-3b89-4aa9-b139-83d5024579f1', 'AddTicket'),
-('f35149b0-b84c-4d80-bc7b-bf8ef71a3aef', 'EditTicket'),
-('2c2ef38a-b3aa-4d1c-b23f-5c89e432ac1f', 'RemoveTicket'),
-('7038f5cd-faf3-4ed5-ae2c-378b0a3940ec', 'AddTicketRelation'),
-('fdc35c17-bf02-47a1-a046-2a913de7d70a', 'EditTicketRelation'),
-('23d3449a-dcd0-4aeb-9331-9828475ab5b2', 'RemoveTicketRelation');
+('4f03786f-6c4e-4b2a-82db-d3ac6aabc144', 'ReadComment'),
+('a4e8f3a1-5df7-4d48-9f02-693a2cdd1d8f', 'WriteComment'),
+('e2d6db94-1a65-4b4f-a4b5-3b54cfd010b6', 'ReadPriority'),
+('021b5b70-45ce-4ea4-94ad-87fd32c5dc73', 'ReadProject'),
+('d9c1f858-4d46-47b0-8c2e-79e4ec1b4a93', 'ReadTag'),
+('9b46c73c-70de-4d51-a5b0-99ef2c5cb37a', 'WriteTag'),
+('fc408726-8cc4-4d5e-9e1e-e0d1c19c7b37', 'ReadTicket'),
+('ab81b6cf-c901-412f-bd0a-f3d385d2e46b', 'WriteTicket'),
+('5e91c771-54ff-4e2c-a40c-f88d7297e2de', 'DeleteTicket'),
+('32c3ddfa-ef1b-419a-9e77-01563f148326', 'ReadTicketRelation'),
+('c1826e4a-7699-4d51-bc57-cae06e3ed8c4', 'WriteTicketRelation'),
+('eb2ea011-3438-495f-820f-9a5e4de8d6e7', 'ReadTicketTag'),
+('3efebff2-7542-4742-8325-0cbb353bc13d', 'WriteTicketTag');
 
 INSERT INTO Priorities (Id, Name, Level) VALUES
 ('9fcb12a4-25de-4e5f-b3e2-17e1ea3cd8e9', 'Undefined', 0),
